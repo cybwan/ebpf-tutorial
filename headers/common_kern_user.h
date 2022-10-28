78d12dea8cea4d6677bc38fe71306d10d5ba5dc3
cybwan <baili@flomesh.io>
2022-10-28 09:40:18 +0800

    Init

---
+++ headers/common_kern_user.h
@@ -0,0 +1,7 @@
+/* This common_kern_user.h is used by kernel side BPF-progs and
+ * userspace programs, for sharing common struct's and DEFINEs.
+ */
+#ifndef __COMMON_KERN_USER_H
+#define __COMMON_KERN_USER_H
+
+#endif /* __COMMON_KERN_USER_H */
